4ec2386ffcb4a55baf8ba10caf52faaea404ca03
iDan <d.mileris@student.fontys.nl>
2025-06-25 05:03:03 +0200

    Added db connectivity

--- API.py
+++ API.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+import pyodbc
 from flask import Flask, request, jsonify
 import logging
 
@@ -8,6 +9,55 @@ logging.basicConfig(level=logging.INFO, filename="status.log", filemode="w",
 
 app = Flask(__name__)
 API_KEY = os.getenv("API_KEY")
+
+def get_db_connection():
+    conn = pyodbc.connect(
+        f"Driver=ODBC Driver 18 for SQL Server;"
+        f"Server=tcp:khsqlserver9745.database.windows.net,1433;"
+        f"Database=khdatabase;"
+        f"Uid=SqlAdmin;"
+        f"Pwd=Password234#;"
+        f"Encrypt=yes;"
+        f"TrustServerCertificate=no;"
+        f"Connection Timeout=30;"
+    )
+    return conn
+
+def send_logs(data):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(f"""INSERT INTO dbo.Metrics (
+                    Timestamp,
+                    DiskFreeGB,
+                    MemoryFreeMB,
+                    MemoryTotalMB,
+                    RamUsagePercent,
+                    RamUsedGB,
+                    CpuPercent,
+                    TcpEstablished,
+                    SshOpen,
+                    Samba139,
+                    Samba445
+                )
+                VALUES (
+                    {data[Timestamp]},
+                    {data[DiskFreeGB]},
+                    {data[MemoryFreeMB]},
+                    {data[MemoryTotalMB]},
+                    {data[RamUsagePercent]},
+                    {data[RamUsedGB]},
+                    {data[CpuPercent]},
+                    {data[TcpEstablished]},
+                    {data[SshOpen]},
+                    {data[Samba139]},
+                    {data[Samba445]}
+                );""")
+
+    conn.commit()
+    logging.info("Data pushed to DB")
+    cursor.close()
+    conn.close()
+
 
 @app.route('/', methods=['POST'])
 def receive_data():
@@ -19,6 +69,8 @@ def receive_data():
 
     data = request.get_json()
     logging.info(data)
+
+    send_logs(data)
 
     return ({"status": "success"}), 200
 
